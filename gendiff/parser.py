3d11258ce28cf8eac6f2f3dd067cc7a2f93dd2d3
Anna Piankova <anny.pyankova@gmail.com>
2023-09-11 22:30:23 +0100

    add yaml

---
+++ gendiff/parser.py
@@ -0,0 +1,9 @@
+import json
+import yaml
+
+
+def parse(data, format_name):
+    if format_name == '.json':
+        return json.load(open(data))
+    if format_name in ('.yml', '.yaml'):
+        return yaml.safe_load(open(data))
